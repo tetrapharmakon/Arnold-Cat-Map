19c6b92a7b97a2b3bff48c8422ae1bc7d628e91b
fouche <tetrapharmakon@gmail.com>
2020-12-17 18:23:27 +0200

    a blind, automatic commit

--- Arnold_Cat_Map.py
+++ Arnold_Cat_Map.py
@@ -6,30 +6,26 @@
 import cv2
 import numpy as np
 
-def run():
-    img = cv2.imread('img/dexter.png')
+IMAGE = 'img/mein-schlong.jpg'
 
-    for i in range (1,26):
-        img = transform(img, i)
+def run():
+  img = cv2.imread(IMAGE)
+  h , _ , _ = img.shape
+  for i in range (1,302):
+    img = transform(img, i)
 
 def transform(img, num):
-
-    rows, cols, ch = img.shape
-    if (rows == cols):
-        n = rows
-        img2 = np.zeros([rows, cols, ch])
-
-        for x in range(0, rows):
-            for y in range(0, cols):
-
-                img2[x][y] = img[(x+y)%n][(x+2*y)%n]
-
-        cv2.imwrite("img/iteration" + str(num) + ".jpg", img2)
-        return img2
-
-    else:
-        print("The image is not square.")
-
+  rows, cols, ch = img.shape
+  if (rows == cols):
+    n = rows
+    img2 = np.zeros([rows, cols, ch])
+    for x in range(0, rows):
+      for y in range(0, cols):
+        img2[x][y] = img[(x+y)%n][(x+2*y)%n]
+    cv2.imwrite("img/iteration" + str(num) + ".jpg", img2)
+    return img2
+  else:
+    print("The image is not square.")
 
 if __name__ == "__main__":
-    run()
+  run()
